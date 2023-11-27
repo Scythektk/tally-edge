d75b86ed760169ed4c72df055fb99896e7639372
puja-maheshwari <151019359+puja-maheshwari@users.noreply.github.com>
2023-11-27 20:56:12 +0530

    Update controller.js

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,10 +1,33 @@
 const pool = require('../../db');
 
-const getUsers = (req, res) => {
-	pool.query("SELECT * FROM users", (error, results)=>{
-		if (error) throw error;
-		res.status(200).json(results.rows);
-	})
-}
+const getUsers = (req, res) => 
+	{
+		pool.query(SELECT * FROM USER WHERE user_name="Alice Smith", (error, results)=>
+		{
+			if (error) throw error;
+			res.status(200).json(results.rows);
+		})
+	}
+
+const geAccountDetails = (req,req) =>
+	{
+		pool.query(SELECT * FROM ACCOUNT, (error,results)=>
+		{
+			if (error) throw error;
+			res.status(200).json(results.rows);
+		})
+	}
+
+const CONSENT = (req,req) =>
+	{
+		pool.query(SELECT * FROM CONSENT, getUsers WHERE CONSENT.user_id=getUSERS.user_id, (error,results)=>
+		{
+			if (error) throw error;
+			res.status(200).json(results.rows);
+		})
+	}
+			
+		
+
 
 module.exports = {getUsers};
